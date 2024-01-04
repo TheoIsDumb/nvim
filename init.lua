d8bdce1febbf39f83dad02ef9f90dd263a7fdc61
theo <lesgooo1993@proton.me>
2024-01-04 23:33:03 +0530

    simplified telescope keybinds

--- init.lua
+++ init.lua
@@ -124,8 +124,8 @@ vim.keymap.set('n', '<Space>t', '<Cmd>ToggleTerm<CR>', { silent = true }) -- tog
 
 -- telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 
 vim.keymap.set('n', ':qq<CR>', ':qa<CR>', { noremap = true, silent = true }) -- close everything
 vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true }) -- line-by-line traversal through paragraphs
